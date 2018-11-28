d9987dd9f058994b7eea6e2e8683dac254243f00
Manikaran20 <mani.da1739@gmail.com>
2018-11-28 16:29:35 +0530

    added static files

--- rightprice/urls.py
+++ rightprice/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth.views import login
 
 app_name = 'rightprice'
 
 urlpatterns = [
 url(r'^$', views.IndexView.as_view(), name='index'),
 url(r'^Car$', views.Car, name='Car'),
-url(r'^House$', views.House, name='House')
+url(r'^House$', views.House, name='House'),
+url(r'^login/$', login, {'template_name': 'rightprice/login.html'} )
 ]
